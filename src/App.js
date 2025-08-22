ee4519a6be915cbae34052f014dba6de8058c594
m9iv <aethawalka@gmail.com>
2025-08-22 15:11:44 +0300

    Add redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import AccountOperations from "./features/accounts/AccountOperations"
+import BalanceDisplay from "./features/accounts/BalanceDisplay"
+import CreateCustomer from "./features/customers/CreateCustomer"
+import Customer from "./features/customers/Customer"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>🏦 The React-Redux Bank ⚛️</h1>
+      <CreateCustomer />
+      <Customer />
+      <AccountOperations />
+      <BalanceDisplay />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
